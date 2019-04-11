2b5132afe31647a798c2602b214671fd8cbf42b0
Roger Kuo <roger9kuo@berkeley.edu>
2019-04-11 16:07:50 -0700

    Updated

---
+++ js/SimpleGraph.js
@@ -0,0 +1,28 @@
+'use strict'
+
+// Do we need these?
+const { CircleNode } = require('./CircleNode')
+const { DiamondNode } = require('./DiamondNode')
+const { LineEdge } = require('./LineEdge')
+
+function SimpleGraph () {
+  return {
+    getNodePrototypes: () => {
+      let nodeTypes = []
+      // Add Node Types Here
+      nodeTypes.push(CircleNode('white'))
+      nodeTypes.push(DiamondNode('white'))
+      return nodeTypes
+    },
+    getEdgePrototypes: () => {
+      let edgeTypes = []
+      // Add Edge Types Here
+      edgeTypes.push(LineEdge())
+      return edgeTypes
+    }
+  }
+}
+
+module.exports = {
+  SimpleGraph
+}
