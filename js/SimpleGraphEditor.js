84dbd0cc29d5e36f02e63c9b1f0a4660e0feec32
Roger Kuo <roger9kuo@berkeley.edu>
2019-04-11 15:54:41 -0700

    Updated

--- js/SimpleGraphEditor.js
+++ js/SimpleGraphEditor.js
@@ -1,10 +1,11 @@
 'use strict'
 
+// Are these two lines necessary?
+// Or will they only be used in the test case file(s)?
 const { GraphFrame } = require('./GraphFrame')
 const { SimpleGraph } = require('./SimpleGraph')
 
 function SimpleGraphEditor () {
-    // TODO
     const frame = GraphFrame(SimpleGraph())
     return frame
 }
