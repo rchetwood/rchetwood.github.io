1750dcc84d933430d8a2676cb2cd8014d9ec860f
Roger Kuo <roger9kuo@berkeley.edu>
2019-04-11 17:18:06 -0700

    Updated - May need edit

--- js/SimpleGraph.js
+++ js/SimpleGraph.js
@@ -5,11 +5,14 @@ const { CircleNode } = require('./CircleNode')
 const { DiamondNode } = require('./DiamondNode')
 const { LineEdge } = require('./LineEdge')
 
+const { Graph } = require('./Graph')
+
 /**
    A simple graph with round nodes and straight edges.
    Extends Graph
 */
 function SimpleGraph () {
+  const sg = Graph()
   return {
     getNodePrototypes: () => {
       let nodeTypes = []
@@ -23,7 +26,37 @@ function SimpleGraph () {
       // Add Edge Types Here
       edgeTypes.push(LineEdge())
       return edgeTypes
-    }
+    },
+    connect: (e, p1, p2) => {
+      return sg.connect(e, p1, p2)
+    },
+    add: (n,p) => {
+      return sg.add(n,p)
+    },
+    findNode: (p) => {
+      return sg.findNode(p)
+    },
+    findEdge: (p) => {
+      return sg.findEdge(p)
+    },
+    draw: (g2) => {
+      return sg.draw(g2)
+    },
+    removeNode: (n) => {
+      return sg.removeNode(n)
+    },
+    removeEdge: (e) => {
+      return sg.removeEdge(e)
+    },
+    getBounds: (g2) => {
+      return sg.getBounds(g2)
+    },
+    getNodes: () => {
+      return sg.nodes
+    },
+    getEdges: () => {
+      return sg.nodes
+    },
   }
 }
 
