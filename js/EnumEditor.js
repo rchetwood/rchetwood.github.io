6ef1f488afe340a3b89aebf67649a5839ddabf24
Xinru <xinrulin99@gmail.com>
2019-04-12 23:35:47 -0700

    finished the skeleton of enumEditor. will edit later on to change it to better fit hard objects

--- js/EnumEditor.js
+++ js/EnumEditor.js
@@ -1,3 +1,22 @@
-function EnumEditor(){
-
+function EnumEditor(cl){
+    let cl = cl
+    return{
+        getTags: () =>{
+            try{
+                //unsure what to do for this line
+                // @SuppressWarnings("unchecked") 
+                //Object[] values = (Object[]) cl.getMethod("values").invoke(null);
+                let values = []
+                values = cl.values()
+                let result = []
+                for(let i = 0; i < values.length; i++){
+                    result[i] = values[i].toString()
+                }
+                return result
+            }
+            catch(err){
+                return undefined
+            }
+        }
+    }
 }
