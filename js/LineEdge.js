88794045bca139931dbfb1860caf6dcfee0979bc
Roger Kuo <roger9kuo@berkeley.edu>
2019-04-18 16:44:53 -0700

    Updated

--- js/LineEdge.js
+++ js/LineEdge.js
@@ -1,35 +1,158 @@
 'use strict'
 
 /**
-   An edge that is shaped like a straight line.
+   An edge that is shaped like a line.
    Extends AbstractEdge in Java
 */
 function LineEdge () {
-  let stroke = 'SOLID'
   let start, end
-  let arrow = {type: undefined, at: undefined}
+  let stroke = 'SOLID'
+  let type = 'LINE'
+  let startArrow = 'NONE'
+  let endArrow = 'NONE'
   return {
     draw: () => {
       const panel = document.getElementById('graphpanel')
-      const line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
       const ps = getConnectionPoints()
-      line.setAttribute('x1', ps.start.x)
-      line.setAttribute('x2', ps.end.x)
-      line.setAttribute('y1', ps.start.y)
-      line.setAttribute('y2', ps.end.y)
-      if (stroke === 'SOLID') {
-        line.setAttribute('stroke-dasharray', 0)
+      // line edge type
+      let line
+      let bend
+      switch (type) {
+        case 'HVEDGE':
+          line = document.createElementNS('http://www.w3.org/2000/svg', 'polyline')
+          bend = {x: ps.end.x, y: ps.start.y}
+          line.setAttribute('points', 
+          '' + ps.start.x + ' ' + ps.start.y + 
+          ' ' + bend.x + ' ' + bend.y + 
+          ' ' + ps.end.x + ' ' + ps.end.y)
+          break;
+        case 'VHEDGE':
+          line = document.createElementNS('http://www.w3.org/2000/svg', 'polyline')
+          bend = {x: ps.start.x, y: ps.end.y}
+          line.setAttribute('points', 
+          '' + ps.start.x + ' ' + ps.start.y + 
+          ' ' + bend.x + ' ' + bend.y + 
+          ' ' + ps.end.x + ' ' + ps.end.y)
+          break;
+        default:
+          line = document.createElementNS('http://www.w3.org/2000/svg', 'line')
+          line.setAttribute('x1', ps.start.x)
+          line.setAttribute('x2', ps.end.x)
+          line.setAttribute('y1', ps.start.y)
+          line.setAttribute('y2', ps.end.y)
       }
-      if (stroke === 'DOTTED') {
-        line.setAttribute('stroke-dasharray', 4)
+      // line stroke types
+      switch (stroke) {
+        case 'DOTTED':
+          line.setAttribute('stroke-dasharray', 4)
+          break;
+        default:
+          line.setAttribute('stroke-dasharray', 0)
       }
-      panel.appendChild(line)
+      // arrow points and types
+      const t = 2.5/lineDist(ps.start,ps.end)
+      let cpt, bpt, rpt, lpt
+      if (startArrow !== 'NONE') {
+        cpt  = linePoint(ps.start,ps.end,t)
+        lpt = rotate(cpt,ps.start,90)
+        bpt = rotate(cpt,ps.start,180)
+        rpt = rotate(cpt,ps.start,270)
+      }
+      if (endArrow !== 'NONE') {
+        cpt  = linePoint(ps.end,ps.start,t)
+        lpt = rotate(cpt,ps.end,90)
+        bpt = rotate(cpt,ps.end,180)
+        rpt = rotate(cpt,ps.end,270)
+      }
+      switch (startArrow) {
+        case 'diamond':
+          sa = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+          sa.setAttribute('fill', 'white')
+          sa.setAttribute('points', 
+          '' + ps.start.x + ' ' + ps.start.y + 
+          ' ' + lpt.x + ' ' + lpt.y + 
+          ' ' + bpt.x + ' ' + bpt.y + 
+          ' ' + rpt.x + ' ' + rpt.y)
+          break;
+        case 'openArrow':
+          sa = document.createElementNS('http://www.w3.org/2000/svg', 'polyline')
+          sa.setAttribute('points', 
+          '' + rpt.x + ' ' + rpt.y +
+          ' ' + ps.start.x + ' ' + ps.start.y + 
+          ' ' + lpt.x + ' ' + lpt.y)
+          break;
+        case 'closeArrow':
+          sa = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+          sa.setAttribute('fill', 'white')
+          sa.setAttribute('points', 
+          '' + rpt.x + ' ' + rpt.y +
+          ' ' + ps.start.x + ' ' + ps.start.y + 
+          ' ' + lpt.x + ' ' + lpt.y)
+          break;
+        default:
+          sa = undefined
+      }
+      switch (endArrow) {
+        case 'diamond':
+          ea = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+          ea.setAttribute('fill', 'white')
+          ea.setAttribute('points', 
+          '' + ps.end.x + ' ' + ps.end.y + 
+          ' ' + lpt.x + ' ' + lpt.y + 
+          ' ' + pt.x + ' ' + pt.y + 
+          ' ' + rpt.x + ' ' + rpt.y)
+          break;
+        case 'openArrow':
+          ea = document.createElementNS('http://www.w3.org/2000/svg', 'polyline')
+          ea.setAttribute('points', 
+          '' + rpt.x + ' ' + rpt.y +
+          ' ' + ps.end.x + ' ' + ps.end.y + 
+          ' ' + lpt.x + ' ' + lpt.y)
+          break;
+        case 'closeArrow':
+          ea = document.createElementNS('http://www.w3.org/2000/svg', 'polygon')
+          ea.setAttribute('fill', 'white')
+          ea.setAttribute('points', 
+          '' + rpt.x + ' ' + rpt.y +
+          ' ' + ps.end.x + ' ' + ps.end.y + 
+          ' ' + lpt.x + ' ' + lpt.y)
+          break;
+        default:
+          ea = undefined
+      }
+      if (sa !== undefined) {
+        panel.appendChild(sa)
+      }
+      if (sa !== undefined) {
+        panel.appendChild(ea)
+      }
+      if (sa !== undefined) {
+        panel.appendChild(line)
+      }
     },
     contains: (aPoint) => {
       const MAX_DIST = 2
       return ptSegDist(getConnectionPoints(), aPoint) < MAX_DIST
     },
+    rotate: (c,p,angle) => {
+      // rotate p by angle at c
+      let radians = (Math.PI / 180) * angle
+      let cos = Math.cos(radians)
+      let sin = Math.sin(radians)
+      let nx = (cos * (p.x - c.x)) + (sin * (p.y - c.y)) + c.x
+      let ny = (cos * (p.y - c.y)) - (sin * (p.x - c.x)) + c.y
+      return {x: nx, y: ny}
+    },
+    linePoint: (p1,p2,t) => {
+      // Find point on line by t distance from p1
+      return {x: (1-t)*p1.x + t*p2.x, y: (1-t)*p1.y + t*p2.y}
+    },
+    lineDist: (p1,p2) => {
+      // Find distance of line from p1 to p2
+      return Math.sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2)
+    },
     ptSegDist: (ps,p) => {
+      // distance between p and line of ps
       let line = (ps.start.x - ps.end.x)**2 + (ps.start.y - ps.end.y)**2 
       if (line === 0){
         return (p.x - ps.start.x)**2 + (p.y - ps.start.y)**2 
