67b8311dd207c2a0352fd8ba5e37e2bf6ecd2444
Roger Kuo <roger9kuo@berkeley.edu>
2019-04-20 19:10:07 -0700

    Graph minor fix - cannot use 'this' notation

--- js/Graph.js
+++ js/Graph.js
@@ -2,7 +2,7 @@
 
 function findNode (p, nodes) {
     for(let i = nodes.length -1; i >= 0; i--){
-        const n = this.nodes[i]
+        const n = nodes[i]
         if(n.contains(p)) return n
     }
     return undefined
@@ -32,29 +32,29 @@ function Graph(){
         } ,
         findNode: (p) =>{
             for(let i = nodes.length -1; i >= 0; i--){
-                const n = this.nodes[i]
+                const n = nodes[i]
                 if(n.contains(p)) return n
             }
             return undefined
         },
         findEdge: (p) =>{
             for(let i = edges.length -1; i >= 0; i--){
-                const e = this.edges[i]
+                const e = edges[i]
                 if(e.contains(p)) return e
             }
             return undefined
         },
         draw: () =>{
-            for (const n of this.nodes) {
+            for (const n of nodes) {
                 n.draw()
             }
-            for (const e of this.edges) {
+            for (const e of edges) {
                 e.draw()
             }
         },
         removeNode: (n) =>{
             for(let i = edges.length -1; i >= 0; i--){
-                const e = this.edges[i]
+                const e = edges[i]
                 if(e.getStart() === n || e.getEnd() === n){
                     edges.remove(e)
                 }
@@ -66,23 +66,24 @@ function Graph(){
         },
         getBounds: () =>{
             let r = undefined
-            for(const n of this.nodes){
+            for(const n of nodes){
                 let b = n.getBounds()
                 if(r === undefined){ r = b}
                 else{ r.add(b)}
             }
-            for(const e of this.edges){
+            for(const e of edges){
                 r.add(e.getBounds())
             }
             //unsure if this is the correct JS translation of the java code
             //return r == null ? new Rectangle2D.Double() : r;
+            // kuor: Cannot use this
             return r
         },
         getNodes: () =>{
-            return this.nodes
+            return nodes
         },
         getEdges: () =>{
-            return this.edges
+            return edges
         }
     }
 }
