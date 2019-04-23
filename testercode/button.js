542a1e16c9cbab4132335fe065c1036b7266fb76
Xinru <xinrulin99@gmail.com>
2019-04-22 20:16:38 -0700

    removed unnecessary files

--- testercode/button.js
+++
@@ -1,7 +0,0 @@
-function createButton(x,y, width, height){
-    return{
-        draw: ()=>{
-            <rect  x="x" y="y" width="40" height="40" style="fill:rgb(0,0,255);stroke-width:1;stroke:rgb(0,0,0)" id="circleNodeBtn" onclick="doRectClick()" />
-        }
-    }
-}
